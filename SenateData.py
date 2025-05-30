bd6645c6b1b30cc4a728163270187e9fea674fe7
Petersquinn <quinton.peters@duke.edu>
2025-05-30 15:03:19 -0400

    c

--- SenateData.py
+++ SenateData.py
@@ -12,26 +12,31 @@ color_counts = {
     '#0077B6': 0,  # Dem Blowout
     '#53A2BE': 1,  # Dem Solid 
     '#90E0EF': 1,  # Dem Close
-    '#CAF0F8': 2   # Dem Term-Limited
+    '#CAF0F8': 2,  # Dem Term-Limited
+    '#828282': 5,  # Dem not up for election
+    '#787276': 13,  # Rep not up for election
+
 }
 
+rep_safe = color_counts['#787276']
 rep_unopposed = color_counts['#880D1E']
 rep_blowout = color_counts['#D00000']
 rep_solid = color_counts['#CE4257']
 rep_close = color_counts['#E37696']
 rep_term = color_counts['#FFB5C2']
+dem_safe = color_counts['#828282']
 dem_unopposed = color_counts['#03045E']
 dem_blowout = color_counts['#0077B6']
 dem_solid = color_counts['#53A2BE']
 dem_close = color_counts['#90E0EF']
 dem_term = color_counts['#CAF0F8']
 
-rep_stacked_vals_ordered = [rep_unopposed, rep_blowout, rep_solid, rep_close, rep_term]
-dem_stacked_vals_ordered = [dem_unopposed, dem_blowout, dem_solid, dem_close, dem_term]
-rep_stacked_colors_ordered = ['#880D1E', '#D00000', '#CE4257', "#E37696", '#FFB5C2']
-dem_stacked_colors_ordered = ['#03045E', '#0077B6', '#53A2BE', '#90E0EF', '#CAF0F8']
-labelsRep = ['Unopposed', 'Blowout', 'Solid', 'Close', 'Term-Limited']
-labelsDem = ['Unopposed', 'Blowout', 'Solid', 'Close', 'Term-Limited']
+rep_stacked_vals_ordered = [rep_safe, rep_unopposed, rep_blowout, rep_solid, rep_close, rep_term]
+dem_stacked_vals_ordered = [dem_safe, dem_unopposed, dem_blowout, dem_solid, dem_close, dem_term]
+rep_stacked_colors_ordered = ['#787276', '#880D1E', '#D00000', '#CE4257', "#E37696", '#FFB5C2']
+dem_stacked_colors_ordered = ['#828282', '#03045E', '#0077B6', '#53A2BE', '#90E0EF', '#CAF0F8']
+labelsRep = ['Not up for Election in 2026', 'Unopposed', 'Blowout', 'Solid', 'Close', 'Term-Limited']
+labelsDem = ['Not up for Election in 2026', 'Unopposed', 'Blowout', 'Solid', 'Close', 'Term-Limited']
 
 fig, ax = plt.subplots(figsize=(8, 6))
 
